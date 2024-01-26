341b53f39ff57d349b7cb2f9dd26c582504b6835
Jakub <01149663@pw.edu.pl>
2024-01-26 01:39:02 +0100

    user edit

--- prototype/pages/2_User.py
+++ prototype/pages/2_User.py
@@ -3,14 +3,14 @@ import streamlit as st
 
 conn = sqlite3.connect('database/database.db')
 cursor = conn.cursor()
-
 # users: username, password, email, premium, preferences
 query = "SELECT username, email, premium, preferences FROM users"
-
 cursor.execute(query)
-results = cursor.fetchall()
-
+user = cursor.fetchone()
 conn.close()
 
-st.write("Użytkownicy:")
-st.write(results)
+st.write("Nazwa użytkownika: "+user[0])
+st.write("Adres e-mail: "+user[1])
+if user[2] == 1:
+    st.write("Konto Premium")
+st.write("Preferencje żywnościowe: "+user[3])
